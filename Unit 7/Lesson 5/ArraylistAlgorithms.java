ee56db95fa17bcfd76e552be0178d64acfff9a92
haydayisawsome <isaacman176@gmail.com>
2024-04-23 02:43:25 -0400

    Commit 5

---
+++ Unit 7/Lesson 5/ArraylistAlgorithms.java
@@ -0,0 +1,15 @@
+/**
+ * Author:  Isaac Man
+ * Date:    April 22, 2024
+ * Subject: Arraylist Algorithms
+ */
+
+ import java.util.*;
+ public class ArrayListAlgorithms{
+    public static void main (String[]args){
+
+    }
+    public Arraylist<Integer> reverse(Arraylist<Integer>list){
+        for (int i = )
+    }
+ }
