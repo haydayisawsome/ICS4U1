82ab9545638ab876dc98a183fbe65cf6af071fd0
haydayisawsome <isaacman176@gmail.com>
2024-05-14 15:16:39 -0400

    sgrgrgrgrdgdg

--- Unit 9/Lesson 2/Main.java
+++ Unit 9/Lesson 2/Main.java
@@ -1,6 +1,24 @@
 public class Main {
+    @SuppressWarnings("unused")
     public static void main(String[]args){
+        //Dog d = new Animal();
+        //this doesnt work as an animal is not necessarily a dog
+        BabyDog BabyDog1 = new BabyDog();
+        /*
+        Output: 
+        This is a default constructor of Animal class
+        This is a default constructor of Dog class
+        It is BabyDog default constructor
+         */
+        //this output is the result of traversing to the higher parent classes to the child classes
 
+        new BabyDog();
+        //this will only call the constructor the same way as the line above
+        //however, there are no objects with the assigned address thus you cannot call the methods
+
+        BabyDog babyDog2 = new BabyDog("My dog",8,"John",true);
+
+        BabyDog1.eat();
     }
 }
 class Animal{
@@ -17,6 +35,7 @@ class Animal{
     public Animal(String n, int a){
         name = n;
         age = a;
+        System.out.println("It is an overloaded constructor of Animal");
     }
 
     //methods
@@ -28,7 +47,8 @@ class Animal{
     }
 }
 class Dog extends Animal{
-    String ownerName;
+    @SuppressWarnings("unused")
+    private String ownerName;
     public Dog(){
         super();
         //this calls the constuctor of the parent class
@@ -36,35 +56,43 @@ class Dog extends Animal{
     }
     public Dog(String n, int a, String oN){
         super(n,a);
+        //you cannot access the private variable from animal but u can access it through a constructor
         //super must be the first line in the constructor
         ownerName = oN;
+        System.out.println("It is an overloaded constructor of Dog");
     }
 
     @Override
     //@Override does not do anything to the code
     //It lets the programmers know that this is an overrided method
     public void eat(){
+        super.eat();
         System.out.println("Dog is eating");
         //This is polymorphism as the header of this method is the exact same as the parent method
+        //Action that does a method a different way
+        //Different from method overloading
         //This overrides the parent method of eat()
     }
+
     public void bark(){
         System.out.println("Dog is barking");
     }
 }
 
 class BabyDog extends Dog{
+    @SuppressWarnings("unused")
     private boolean needsMilk;
     public BabyDog(){
         super();
         System.out.println("It is BabyDog default constructor");
     }
-    public BabyDog(String n, int a, String oN){
-        name = n;
-        age = a;
-        ownerName = oN;
+    public BabyDog(String n, int a, String oN, boolean nM){
+        super(n,a,oN);
+        needsMilk = nM;
+        System.out.println("It is an overloaded constructor of BabyDog");
     }
-    public void noMilk(){
-        needsMilk = true;
+    public void eat(){
+        super.eat(); //super can be on any line when calling a method
+        System.err.println("BabyDog is eating");
     }
 }
---
+++ _Poker_/Main.java
@@ -0,0 +1,15 @@
+package _Poker_;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[]args){
+        Main m = new Main(); // Creates an instance of the game to play
+        m.addWindowListener();
+    }
+    public Main(){
+        // Creating and setting the dimensions of the panel
+        JPanel panel = new JPanel();
+    }
+    
+}
