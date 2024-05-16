bc3336a9947293a1ba0f74fa58a3473c6b33caf2
haydayisawsome <isaacman176@gmail.com>
2024-05-15 22:01:51 -0400

    feat: added proper dimensions to the JFrame

--- _Snakes_And_Ladders_/Main.java
+++ _Snakes_And_Ladders_/Main.java
@@ -1,38 +1,53 @@
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Main extends JFrame implements WindowListener, MouseListener{
-    //coordinates of the top left point to offset the frame so that the top left point is 0
-    final static int leftFramePos = 108;
-    final static int topFramePos = 131;
-    final static int rightFramePos = 891;
-    final static int bottomFramePos = 891;
-
     final static int frameHeight = 800;
     final static int frameWidth = 800;
 
+    String event = "Title Screen";
+
     //main method used to create panel and an instance of this class to create a panel of the game
     public static void main(String[]args){
-        Main m = new Main(); // Creates an instance of the game to play
-        m.addWindowListener(m);
-        m.setVisible(true);
+        Main main = new Main();
+        main.setFrame();
+    }
 
-        //for some reason, java positions do not match the pixels
-        //using the experiemented offset values to set actual boundaries of the panel
-        m.setBounds(100,100,frameHeight + (frameHeight - (rightFramePos - leftFramePos)),frameWidth + (frameWidth - (bottomFramePos - topFramePos)));
+    //this method must be an instance method as an error will occur when the code is placed in the static main method
+    public void setFrame(){
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        // For some reason, the dimensions of the frame do not match the variables for setBounds
+        // I have experiemented with the offset numbers to match the dimensions of the JFrame and the variables
+        setBounds(100,100,frameHeight + 12,frameWidth + 35);
     }
 
     //all game code goes in main method
     public Main(){
-        // Creating and setting the dimensions of the panel
-        JPanel panel = new JPanel();
-        add(panel);
+        super("Snakes and Ladders"); //calls the default constructor of the parent class (JFrame) to create a new JFrame
+        addMouseListener(this);
+        addWindowListener(this);
     }
 
     //paint method for all graphics
-    public void paint(Graphics g){
+    public void paint(Graphics G){
+        //detect which event is ongoing and paints screen depending on event
 
+        //this line of code sets the graphics to match the pixels of the JFrame, and not the JPanel
+        Graphics g = getContentPane().getGraphics();
+
+        if(event.equals("Title Screen")){
+            g.setColor(Color.GRAY);
+            g.fillRect(0,0,frameWidth,frameHeight);
+            g.setColor(Color.BLUE);
+            g.fillOval(0,0,100,100);
+            g.fillOval(0,700,100,100);
+            g.fillOval(700,0,100,100);
+            g.fillOval(700,700,100,100);
+            System.out.println(getContentPane().getHeight()+ "   "+getContentPane().getWidth());
+        }
     }
 
     //Abstract methods
