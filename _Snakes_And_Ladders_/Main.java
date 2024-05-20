748510bfeaedae7958eef2bdc2f983c711da3d31
haydayisawsome <isaacman176@gmail.com>
2024-05-20 18:10:32 -0400

    feat: fixed formatting of title screen

--- _Snakes_And_Ladders_/Main.java
+++ _Snakes_And_Ladders_/Main.java
@@ -25,7 +25,7 @@ public class Main extends JFrame implements WindowListener, MouseListener{
         
         // For some reason, the dimensions of the frame do not match the variables for setBounds
         // I have experiemented with the offset numbers to match the dimensions of the JFrame and the variables
-        setBounds(100,100,frameHeight + 16,frameWidth + 39);
+        setBounds(100,100,frameHeight,frameWidth);
     }
 
     //all game code goes in main method
@@ -33,15 +33,27 @@ public class Main extends JFrame implements WindowListener, MouseListener{
         super("Snakes and Ladders"); //calls the default constructor of the parent class (JFrame) to create a new JFrame
         addMouseListener(this);
         addWindowListener(this);
+        setLayout(null);
 
-        // creating a seperate panel with all of the title screen elements
-        // Source: https://forums.codeguru.com/showthread.php?492301-SWING-JLabel-set-background-color 
+        // JLabel source: https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/javax/swing/JLabel.html
         JLabel title = new JLabel("SNAKES AND LADDERS");
         title.setFont(new Font("Arial", Font.BOLD, 60));
-        title.setForeground(Color.BLUE);
-        JPanel titlePanel = new JPanel();
-        titlePanel.setBackground(Color.GRAY);
-        titlePanel.add(title);
+        title.setBounds(100,100,100,100);
+        // Color class source: https://docs.oracle.com/javase/8/docs/api/java/awt/Color.html
+        // Setting JLabel color source: https://forums.codeguru.com/showthread.php?492301-SWING-JLabel-set-background-color 
+        title.setForeground(Color.GREEN.darker());
+
+        JButton playGame = new JButton("Hello");
+        playGame.setSize(new Dimension(200, 100));
+        // horiz alignment for button: https://stackoverflow.com/questions/12589494/align-text-in-jlabel-to-the-right
+        playGame.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        // creating a seperate panel with all of the title screen elements
+        JPanel titlePanel = new JPanel(new BorderLayout(0,0));
+        titlePanel.setBackground(Color.YELLOW);
+        titlePanel.add(title,BorderLayout.NORTH);
+        titlePanel.add(playGame,BorderLayout.CENTER);
+        titlePanel.setBounds(0,0,getX(),getY());
         add(titlePanel);
     }
 
@@ -54,8 +66,8 @@ public class Main extends JFrame implements WindowListener, MouseListener{
         Graphics g = getContentPane().getGraphics();
 
         if(event.equals("Title Screen")){
-            setBackground(Color.GRAY);
-            g.setColor(Color.BLUE);
+            setBackground(Color.YELLOW);
+            g.setColor(Color.GREEN);
         }
         if(event.equals("Game")){
             ImageIcon image = new ImageIcon("C:\\Users\\isaac\\Downloads\\ICS4U1\\ICS4U1\\GUI\\Airplane.png");
