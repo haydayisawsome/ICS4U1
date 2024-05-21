82fc3afad67c74a29a1363d0bae2dfc62a1e8d9c
haydayisawsome <isaacman176@gmail.com>
2024-05-21 18:00:41 -0400

    feat: added options screen

--- _Snakes_And_Ladders_/Main.java
+++ _Snakes_And_Ladders_/Main.java
@@ -1,16 +1,22 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.*;
-
 import javax.swing.*;
 
-public class Main extends JFrame implements WindowListener, MouseListener{
-    final static int frameHeight = 800;
-    final static int frameWidth = 800;
+public class Main extends JFrame implements WindowListener, MouseListener, ActionListener{
+    final static int frameHeight = 814;
+    final static int frameWidth = 814;
 
     String event = "Title Screen";
-    BufferedImage board;
+    int NumOfPlayers = 4;
 
+    // creating seperate panels for different events
+    JPanel titlePanel = new JPanel(new BorderLayout(0,0));
+    JPanel optionsPanel = new JPanel(new BorderLayout(0,0));
+    JPanel gamePanel = new JPanel(new BorderLayout(0,0));
+
+    //Creating a slider to see how many players are playing
+    //Source (including the JSlider methods used in this game): https://www.geeksforgeeks.org/java-swing-jslider/
+    JSlider slider = new JSlider(1,10,4);
 
     //main method used to create panel and an instance of this class to create a panel of the game
     public static void main(String[]args){
@@ -28,13 +34,18 @@ public class Main extends JFrame implements WindowListener, MouseListener{
         setBounds(100,100,frameHeight,frameWidth);
     }
 
-    //all game code goes in main method
+    //all screen's frontend code goes in this method
     public Main(){
         super("Snakes and Ladders"); //calls the default constructor of the parent class (JFrame) to create a new JFrame
         addMouseListener(this);
         addWindowListener(this);
-        setLayout(null);
+        setLayout(null); //all dimensions are based on setBounds
 
+        //ensuring that the other panels in the game are not shown
+        optionsPanel.setVisible(false);
+        gamePanel.setVisible(false);
+
+        //TITLE SCREEN
         // JLabel source: https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/javax/swing/JLabel.html
         JLabel title = new JLabel("SNAKES AND LADDERS");
         title.setFont(new Font("Arial", Font.BOLD, 60));
@@ -42,19 +53,77 @@ public class Main extends JFrame implements WindowListener, MouseListener{
         // Color class source: https://docs.oracle.com/javase/8/docs/api/java/awt/Color.html
         // Setting JLabel color source: https://forums.codeguru.com/showthread.php?492301-SWING-JLabel-set-background-color 
         title.setForeground(Color.GREEN.darker());
+        // horiz alignment source: https://stackoverflow.com/questions/12589494/align-text-in-jlabel-to-the-right
+        title.setHorizontalAlignment(SwingConstants.CENTER);
 
-        JButton playGame = new JButton("Hello");
-        playGame.setSize(new Dimension(200, 100));
-        // horiz alignment for button: https://stackoverflow.com/questions/12589494/align-text-in-jlabel-to-the-right
+        //JButton source: GUI unit, class MovingCar.java
+        JButton playGame = new JButton("SELECT OPTIONS");
+        playGame.setFont(new Font("Arial", Font.BOLD, 30));
+        playGame.addActionListener(this);
+        //setPreferred size is used when there is already a layout manager
+        //this dimension will be considered but might not always work (only works for height and not width due to layout manager)
+        //Source: https://stackoverflow.com/questions/1783793/java-difference-between-the-setpreferredsize-and-setsize-methods-in-compone
+        playGame.setPreferredSize(new Dimension(800,75));
         playGame.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        // creating a seperate panel with all of the title screen elements
-        JPanel titlePanel = new JPanel(new BorderLayout(0,0));
+
+        //getting image from files source: https://stackoverflow.com/questions/16631636/what-is-the-correct-path-to-display-an-imageicon-png-file-for-windows-7
+        ImageIcon boardImgIcon = new ImageIcon("_Snakes_And_Ladders_\\resources\\Board.png"); 
+
+        //setting the image to the label
+        JLabel boardLabel = new JLabel();
+        boardLabel.setIcon(boardImgIcon);
+        boardLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        titlePanel.setVisible(true);
         titlePanel.setBackground(Color.YELLOW);
-        titlePanel.add(title,BorderLayout.NORTH);
-        titlePanel.add(playGame,BorderLayout.CENTER);
-        titlePanel.setBounds(0,0,getX(),getY());
+        titlePanel.add(title,BorderLayout.PAGE_START);
+        titlePanel.add(playGame,BorderLayout.PAGE_END);
+        titlePanel.add(boardLabel,BorderLayout.CENTER);
+        titlePanel.setBounds(0,0,frameWidth,frameHeight-35);
         add(titlePanel);
+
+        //OPTIONS SCREEN
+        JLabel optionsTitle = new JLabel("Choose number of players:");
+        optionsTitle.setFont(new Font("Arial", Font.BOLD, 60));
+        optionsTitle.setBounds(100,100,100,100);
+        optionsTitle.setForeground(Color.GREEN.darker());
+        optionsTitle.setHorizontalAlignment(SwingConstants.CENTER);
+
+        slider.setPreferredSize(new Dimension(frameWidth-100,100));
+        slider.setSnapToTicks(true);
+        slider.setPaintLabels(true);
+        slider.setPaintTrack(true);
+        slider.setPaintTicks(true);
+        slider.setMajorTickSpacing(1);
+        slider.setBackground(Color.YELLOW);
+
+        //creating another panel with just the slider so it can be resized with changable dimensions
+        JPanel sliderPanel = new JPanel();
+        sliderPanel.add(slider);
+        sliderPanel.setBackground(Color.YELLOW);
+
+        JButton playerNumOptionsButton = new JButton("START");
+        playerNumOptionsButton.setFont(new Font("Arial", Font.BOLD, 30));
+        playerNumOptionsButton.addActionListener(this);
+        playerNumOptionsButton.setPreferredSize(new Dimension(800,75));
+        playerNumOptionsButton.setHorizontalAlignment(SwingConstants.CENTER);
+
+        optionsPanel.setBackground(Color.YELLOW);
+        optionsPanel.add(optionsTitle,BorderLayout.PAGE_START);
+        optionsPanel.add(sliderPanel,BorderLayout.CENTER);
+        optionsPanel.add(playerNumOptionsButton,BorderLayout.PAGE_END);
+        optionsPanel.setBounds(0,0,frameWidth,frameHeight-35);
+        add(optionsPanel);
+
+        //GAME PANEL
+        JLabel boardLabel2 = new JLabel();
+        boardLabel2.setIcon(boardImgIcon);
+        boardLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+
+        gamePanel.setBackground(Color.YELLOW);
+        gamePanel.add(boardLabel2);
+        gamePanel.setBounds(0,0,frameWidth,frameHeight-35);
+        add(gamePanel);
     }
 
     //paint method for all graphics
@@ -65,13 +134,26 @@ public class Main extends JFrame implements WindowListener, MouseListener{
         super.paint(G);
         Graphics g = getContentPane().getGraphics();
 
-        if(event.equals("Title Screen")){
-            setBackground(Color.YELLOW);
-            g.setColor(Color.GREEN);
-        }
         if(event.equals("Game")){
-            ImageIcon image = new ImageIcon("C:\\Users\\isaac\\Downloads\\ICS4U1\\ICS4U1\\GUI\\Airplane.png");
-            image.paintIcon(this,g,0,0);
+
+        }
+    }
+
+    //all code after the start button is here
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand(); //source: GUI unit, MovingCar.java class
+        if (s.equals("SELECT OPTIONS")){
+            //changing visible panel to options screen
+            titlePanel.setVisible(false);
+            optionsPanel.setVisible(true);
+
+            //detecting the different buttons pressed in options screen
+
+        }
+        if(s.equals("START")){
+            NumOfPlayers = slider.getValue();
+            optionsPanel.setVisible(false);
+            gamePanel.setVisible(true);
         }
     }
 
