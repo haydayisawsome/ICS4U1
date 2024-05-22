e4d79f8a81c5ce4dfd610742f69a146aa9622f5e
haydayisawsome <isaacman176@gmail.com>
2024-05-22 14:20:18 -0400

    fffeffdff

---
+++ Unit 9/Lesson 4/Employee.java
@@ -0,0 +1,33 @@
+public class Employee extends Person{
+    //instance variables
+    private int empID;
+
+    //overloaded constructor
+    public Employee(String n,String g,int e){
+        super(n,g);
+        empID = e;
+    }
+    @Override
+    public void work(){
+        if(empID == 0){
+            System.out.println("Not working");
+        }
+        else{
+            System.out.println("Working as employee");
+        }
+    }
+    @Override
+    public String toString(){
+        return super.toString() + ", Employee ID: " + empID;
+    }
+
+    public static void main(String[] args) {
+        //Person p = new Person("Minoo","Female",0); //doesnt work for person, only works for employee
+        Person student = new Employee("Minoo","Female",0);
+        System.out.println(student);
+        student.work();
+        Person employee = new Employee("John","Male",1234);
+        System.out.println(employee);
+        employee.work();
+    }
+}
